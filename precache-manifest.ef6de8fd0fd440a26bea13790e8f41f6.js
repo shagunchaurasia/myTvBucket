056979cb77081480039d5fcd3597430f91438496
shagunchaurasia <chaurasia.shagun@gmail.com>
2020-07-29 00:05:30 +0900

    Updates

---
+++ precache-manifest.ef6de8fd0fd440a26bea13790e8f41f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fef24494877e7ee8502c6a386198bb9",
+    "url": "/github.io/myTvBucket/index.html"
+  },
+  {
+    "revision": "330579345fb9c93312af",
+    "url": "/github.io/myTvBucket/static/css/main.97f15fb6.chunk.css"
+  },
+  {
+    "revision": "fd10eb53486a07a34c66",
+    "url": "/github.io/myTvBucket/static/js/2.125e8768.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/myTvBucket/static/js/2.125e8768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330579345fb9c93312af",
+    "url": "/github.io/myTvBucket/static/js/main.7de3ccab.chunk.js"
+  },
+  {
+    "revision": "b02e2cccb0710fe975e6",
+    "url": "/github.io/myTvBucket/static/js/runtime-main.2fc72541.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/myTvBucket/static/media/logo.5d5d9eef.svg"
+  }
+]);
